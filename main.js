0cee18be7f4b8ae1ce8be05be5d63d9ae7df884d
Sanvi9 <74234358+Sanvi9@users.noreply.github.com>
2021-08-15 18:45:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX = 0;
+noseY = 0;
+function preload() {
+    img = loadImage("https://i.postimg.cc/9FTQN32f/clownnose.png")
+}
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses)
+}
+function modelLoaded() {
+    console.log("modelLoaded")
+
+}
+
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log(noseX,noseY)
+;    }
+}
+function takeSnapshot() {
+    save("FilterImage.png")
+}
+
+function draw() {
+    image(video,0,0,300,300);
+    //fill("red")
+    //circle(noseX,noseY,25);
+    image(img,noseX -10,noseY,25,25)
+}
